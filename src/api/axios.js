a95c1cf4be8d993e58a351f9a942a8a9d80dc3bc
Datthehooman <datbui13579@gmail.com>
2025-11-20 08:22:35 +0700

    tự đông lấy token từ client

--- src/api/axios.js
+++ src/api/axios.js
@@ -5,34 +5,22 @@ const apiClient = axios.create({
     timeout: 10000
 });
 
-// // Interceptor cho request
-// apiClient.interceptors.request.use(
-//     (config) => {
-//         const authStore = useAuthStore();
-//         if (authStore.token) {
-//             config.headers.Authorization = `Bearer ${authStore.token}`;
-//         }
-//         return config;
-//     },
-//     (error) => Promise.reject(error)
-// );
+// helper to get token from cookie
+function getTokenFromCookie(name) {
+    const match = document.cookie.match(new RegExp('(^| )' + name + '=([^;]+)'));
+    return match ? match[2] : null;
+}
 
-// // Interceptor cho response
-// apiClient.interceptors.response.use(
-//     (response) => response,
-//     async (error) => {
-//         const authStore = useAuthStore();
-
-//         // If 401, clear auth and redirect to login
-//         if (error.response && error.response.status === 401) {
-//             authStore.clearAuth();
-//             if (window.location.pathname !== '/auth/login') {
-//                 window.location.href = '/auth/login';
-//             }
-//         }
-
-//         return Promise.reject(error);
-//     }
-// );
+// attach token automatically
+apiClient.interceptors.request.use((config) => {
+    const token = getTokenFromCookie('token');
+    if (token) {
+        config.headers = {
+            ...config.headers,
+            Authorization: `Bearer ${token}`
+        };
+    }
+    return config;
+});
 
 export default apiClient;
