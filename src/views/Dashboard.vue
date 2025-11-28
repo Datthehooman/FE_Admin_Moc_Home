c53f6c751d9c1663a6d5f69f7a8a21d3eee00d25
Datthehooman <datbui13579@gmail.com>
2025-11-28 20:22:45 +0700

    thêm thư viện tạo bài viết

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -4,19 +4,28 @@ import NotificationsWidget from '@/components/dashboard/NotificationsWidget.vue'
 import RecentSalesWidget from '@/components/dashboard/RecentSalesWidget.vue';
 import RevenueStreamWidget from '@/components/dashboard/RevenueStreamWidget.vue';
 import StatsWidget from '@/components/dashboard/StatsWidget.vue';
+
+import { MdEditor } from 'md-editor-v3';
+import 'md-editor-v3/lib/style.css';
+import { ref } from 'vue';
+
+const text = ref('Hello Editor!');
 </script>
 
 <template>
-    <div class="grid grid-cols-12 gap-8">
-        <StatsWidget />
+    <div>
+        <MdEditor v-model="text" language="en" />
+        <div class="grid grid-cols-12 gap-8">
+            <StatsWidget />
 
-        <div class="col-span-12 xl:col-span-6">
-            <RecentSalesWidget />
-            <BestSellingWidget />
-        </div>
-        <div class="col-span-12 xl:col-span-6">
-            <RevenueStreamWidget />
-            <NotificationsWidget />
+            <div class="col-span-12 xl:col-span-6">
+                <RecentSalesWidget />
+                <BestSellingWidget />
+            </div>
+            <div class="col-span-12 xl:col-span-6">
+                <RevenueStreamWidget />
+                <NotificationsWidget />
+            </div>
         </div>
     </div>
 </template>
